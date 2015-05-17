5762db66096854f379e8b0d9f3a0b5124da5c7b8
Bhawani Singh Choudhary <choudharybsc@gmail.com>
2015-05-17 22:02:24 +0530

    working code for BT

--- app/src/main/java/com/example/bhawani_pt538/adaptersbasics/OrderHistory.java
+++ app/src/main/java/com/example/bhawani_pt538/adaptersbasics/OrderHistory.java
@@ -1,26 +1,38 @@
 package com.example.bhawani_pt538.adaptersbasics;
 
 import android.app.ActionBar;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
+//imports for bluetooth
+//imports for bluetooth
 
 public class OrderHistory extends ActionBarActivity {
     List<String> listDataHeader;
@@ -28,14 +40,46 @@ public class OrderHistory extends ActionBarActivity {
     LinkedHashMap<Integer,LinkedHashMap<Integer,LinkedHashMap<Integer,Integer>>> orderGrAndChildIndex;
     LinkedHashMap<Integer,LinkedHashMap<Integer,List<String>>> indexNameMap;
     public static SQLiteDatabase sqLiteDatabase;
+    //bluetooth module variables
+    TextView myLabel;
+    EditText myTextbox;
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
 
+    //bluetooth module variable
+    private Handler mHandler = new Handler();
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            // Do some stuff that you want to do here
 
+            // You could do this call if you wanted it to be periodic:
+            mHandler.postDelayed(this, 5000 );
 
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_history);
+        // bluetooth HC-05
+
+        findBT();
+        try {
+            openBT();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // bluetooth HC-05
 
         sqLiteDatabase = openOrCreateDatabase("systemdb", MODE_PRIVATE,null);
         prepareListDataFromDB();
@@ -68,6 +112,11 @@ public class OrderHistory extends ActionBarActivity {
         }
 
         final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
+        String btOrderNo = ""; //for arduino bluetooth
+        List<String> lastOrderItems = new ArrayList<>();
+        List<String> noOfItems = new ArrayList<>();
+
         for(Integer orderNO:orderGrAndChildIndex.keySet()) {
 //            Log.d("orderNo",""+orderNO);
             float total = 0;
@@ -75,7 +124,12 @@ public class OrderHistory extends ActionBarActivity {
             temLinearLayout.setOrientation(LinearLayout.VERTICAL);
             TextView orderNoTextView = new TextView(this);
             orderNoTextView.setBackgroundColor(Color.GRAY);
-            orderNoTextView.setText("Order Number :"+orderNO);
+            orderNoTextView.setText("Order Number :" + orderNO);
+
+            btOrderNo = String.valueOf(orderNO);
+            lastOrderItems = new ArrayList<>();
+            noOfItems = new ArrayList<>();
+
             temLinearLayout.addView(orderNoTextView);
             if(orderGrAndChildIndex.containsKey(orderNO))
                 if(orderGrAndChildIndex.get(orderNO).keySet() != null)
@@ -99,6 +153,8 @@ public class OrderHistory extends ActionBarActivity {
                         chNameTextView.setBackgroundColor(Color.WHITE);
                         chNameTextView.setText(menuCursor.getString(0));
                         horLinearLayout.addView(chNameTextView);
+                        lastOrderItems.add(menuCursor.getString(0));
+
 
                         chNameTextView = new TextView(this);
                         chNameTextView.setLayoutParams(new TableLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT,0.2f));
@@ -111,6 +167,7 @@ public class OrderHistory extends ActionBarActivity {
                         chNameTextView.setBackgroundColor(Color.MAGENTA);
                         chNameTextView.setText("" + orderGrAndChildIndex.get(orderNO).get(grIndex).get(childIndex));
                         horLinearLayout.addView(chNameTextView);
+                        noOfItems.add(String.valueOf(orderGrAndChildIndex.get(orderNO).get(grIndex).get(childIndex)));
 
                         chNameTextView = new TextView(this);
                         chNameTextView.setLayoutParams(new TableLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT,0.2f));
@@ -136,6 +193,21 @@ public class OrderHistory extends ActionBarActivity {
 
         }
 
+        String btDataSend = "*Order No: ";
+        btDataSend += btOrderNo;
+        btDataSend += "$";
+        int temCount = 0;
+        for(String itemName:lastOrderItems) {
+            btDataSend += (temCount+1)+" "+itemName + " " + noOfItems.get(temCount)+"$";
+            temCount++;
+        }
+
+        try {
+            sendData(btDataSend);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
 //        scrollView.fullScroll(ScrollView.FOCUS_DOWN);
 
@@ -158,6 +230,13 @@ public class OrderHistory extends ActionBarActivity {
                 scrollView.fullScroll(View.FOCUS_DOWN);
             }
         });
+
+//        mHandler.postDelayed(mUpdateTimeTask, 5000);
+        try {
+            closeBT();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -250,5 +329,124 @@ public class OrderHistory extends ActionBarActivity {
                 }
 
         }
+    }
+
+
+    //bluetooth connection
+
+    void findBT()
+    {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null)
+        {
+//            myLabel.setText("No bluetooth adapter available");
+        }
+
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals("HC-05"))
+                {
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+//        myLabel.setText("Bluetooth Device Found");
+    }
+
+    void openBT() throws IOException
+    {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        mmSocket.connect();
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
+
+//        beginListenForData();
+
+//        myLabel.setText("Bluetooth Opened");
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+//                                            myLabel.setText(data);
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    void sendData(String msg) throws IOException
+    {
+//        String msg = "this is right";//myTextbox.getText().toString();
+//        msg += "\n";
+        mmOutputStream.write(msg.getBytes());
+//        myLabel.setText("Data Sent");
+    }
+
+    void closeBT() throws IOException
+    {
+        stopWorker = true;
+        mmOutputStream.close();
+        mmInputStream.close();
+        mmSocket.close();
+//        myLabel.setText("Bluetooth Closed");
     }
 }
